aadc802a0b4422a8d65831d8f0cdd564cb967154
JustinSheppard <52614441+JustinSheppard@users.noreply.github.com>
2020-03-18 04:48:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ request.onload = function() {
 
   if (request.status >= 200 && request.status < 400) {
     data.competitors.forEach(competitor => {
-			//console.log(competitor);
+			console.log(competitors.competitor.name);
 			
 			
       const card = document.createElement('div');
