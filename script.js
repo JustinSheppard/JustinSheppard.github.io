a1fc3eaff9dc6808b41ee6337ac9a056bfd9d1f4
JustinSheppard <52614441+JustinSheppard@users.noreply.github.com>
2020-03-18 01:31:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ request.onload = function() {
   var data = JSON.parse(this.response)
 
   if (request.status >= 200 && request.status < 400) {
-    data.competitor.forEach(competitor => {
+    data.competitors.competitor.forEach(competitor => {
 			console.log(competitor.name)
       const card = document.createElement('div')
 			card.setAttribute('class', 'card')
