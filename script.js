7beaa49b5a88fda2cb900b1764251822a5bf37bb
JustinSheppard <52614441+JustinSheppard@users.noreply.github.com>
2020-03-18 00:30:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+const app = document.getElementById('root')
+const logo = document.createElement('img')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+
 var request = new XMLHttpRequest()
 
 request.open('GET', 'https://api.overwatchcontenders.com/teams', true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
 
-request.onload = function(){
-	var data = JSON.parse(this.response)
-	
-	data.forEach(movie => {
-		
-  console.log(movie.title)
-})
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(competitor => {
+      const card = document.createElement('div')
+			card.setAttribute('class', 'card')
+			
+			const h1 = document.createElement('h1')
+			h1.textContent = competitor.name
+			
+			const p = document.createElement('p')
+			competitor.game = competitor.game.substring(0, 300)
+			p.textContent = '${competitor.name}...'
+			
+			container.appendChild(card)
+			card.appendChild(h1)
+			card.appendChild(p)
+    })
+  } else {
+    console.log('error')
+  }
 }
 
-request.send();
+request.send()
 
