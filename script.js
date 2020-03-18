72ca02a92c5a1ad3a9e98f69ac71bfaa0d2e8a6a
JustinSheppard <52614441+JustinSheppard@users.noreply.github.com>
2020-03-18 19:29:58 +0000

    Update

--- script.js
+++ script.js
@@ -8,38 +8,13 @@ app.appendChild(container);
 
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://api.overwatchcontenders.com/teams', true);
+request.open('GET', 'https://api.overwatchleague.com/v2/teams', true);
 request.onload = function () {
     // Begin accessing JSON data here
-    var data = JSON.parse(this.response); //everything
-    var competitors = data.competitors;
+    var data = JSON.parse(this.response);
 
     if (request.status >= 200 && request.status < 400) {
-        for (var i in competitors) {
-            var competitor = competitors[1][1];
-            //var name = competitor.name;
-            console.log(competitor);
-            //console.log(name);
-
-
-            /*const card = document.createElement('div');
-                  card.setAttribute('class', 'card');
-
-                  const h1 = document.createElement('h1');
-            h1.textContent = competitor.name;
-
-                  //console.log(h1);
-                  const p = document.createElement('p');
-                  p.textContent = competitor.game;
-
-                  //competitor.game = competitor.game.substring(0, 300)
-                  //p.textContent = '${competitor.name}...'
-
-                  container.appendChild(card);
-                  card.appendChild(h1);
-                  card.appendChild(p);*/
-        }
-        /*data.competitors.forEach(competitor => {
+        data.forEach(competitor => {
                 console.log(competitor.name);
 
 
@@ -59,7 +34,7 @@ request.onload = function () {
                 container.appendChild(card);
                 card.appendChild(h1);
                 card.appendChild(p);
-        });*/
+        });
     } else {
         const errorMessage = document.createElement('marquee');
         errorMessage.textContent = `Gah, it's not working!`;
