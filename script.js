94fb8b3b06afb87acde59e4626c9131cb1ffc071
JustinSheppard <52614441+JustinSheppard@users.noreply.github.com>
2020-03-18 03:45:09 +0000

    bug fixing

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-const app = document.getElementById('root')
-const logo = document.createElement('img')
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+const app = document.getElementById('root');
+const logo = document.createElement('img');
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
 
-app.appendChild(logo)
-app.appendChild(container)
+app.appendChild(logo);
+app.appendChild(container);
 
 
-var request = new XMLHttpRequest()
-request.open('GET', 'https://api.overwatchcontenders.com/teams', true)
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.overwatchcontenders.com/teams', true);
 request.onload = function() {
   // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
+  var data = JSON.parse(this.response);
 
   if (request.status >= 200 && request.status < 400) {
     data.competitors.forEach(competitor => {
-			console.log(competitor.name)
-      const card = document.createElement('div')
-			card.setAttribute('class', 'card')
+			//console.log(competitor.name);
+      const card = document.createElement('div');
+			card.setAttribute('class', 'card');
 			
-			const h1 = document.createElement('h1')
-			h1.textContent = competitor.name
+			const h1 = document.createElement('h1');
+			h1.textContent = competitor.name;
 			
-			const p = document.createElement('p')
-			p.textContent = competitor.game
+			const p = document.createElement('p');
+			p.textContent = competitor.game;
 			
 			//competitor.game = competitor.game.substring(0, 300)
 			//p.textContent = '${competitor.name}...'
 			
-			container.appendChild(card)
-			card.appendChild(h1)
-			card.appendChild(p)
-    })
+			container.appendChild(card);
+			card.appendChild(h1);
+			card.appendChild(p);
+    });
   } else {
-    console.log('error')
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
   }
-}
+};
 
-request.send()
+request.send();
 
