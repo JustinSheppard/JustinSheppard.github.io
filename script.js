b93c7baa625dfaef5b71ad6f28ca53ee51daf85e
JustinSheppard <52614441+JustinSheppard@users.noreply.github.com>
2020-03-19 23:53:39 +0000

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const app = document.getElementById('root');
 const logo = document.createElement('img');
+logo.src = 'https://bnetcmsus-a.akamaihd.net/cms/page_media/Q8TMKNUFIJL51519747890664.svg';
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
